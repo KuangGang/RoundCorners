a3b498a2611d922a004bf5cd9f24c0d19b57c182
kuanggang <kuanggang_android@163.com>
2019-12-02 18:17:23 +0800

    first commit

--- app/src/main/java/com/kproduce/roundcornerviews/MainActivity.java
+++ app/src/main/java/com/kproduce/roundcornerviews/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
